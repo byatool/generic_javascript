755cbf3956a28dc4ed2e0f3d66959a8b5fa904ed
sean <sean@byatool.com>
2013-04-06 08:26:07 -0700

    New version of the validation dsl is complete

--- content/script/live/validation/validationInterpreter.js
+++ content/script/live/validation/validationInterpreter.js
@@ -10,20 +10,20 @@ goog.provide('src.site.validation.validationInterpreter');
  @export
  */
 src.site.validation.validationInterpreter.interpret = function(rules, methods) {
-    var methodsToCall = goog.array.map(rules, function(currentRule){
+    var methodsToCall = goog.array.map(rules, function(currentRule) {
         var methodToUse = goog.array.find(methods, function(method) {
             return method[0] === currentRule[0];
         })[1];
-        
+
         var toCheck = goog.array.splice(currentRule, 1);
-        
-        var toCall = goog.array.map(toCheck, function(innerRule){
+
+        var toCall = goog.array.map(toCheck, function(innerRule) {
             return methodToUse(innerRule);
         });
-        
+
         return toCall;
     });
-    
+
     return goog.array.flatten(methodsToCall);
 };
 
@@ -35,23 +35,23 @@ src.site.validation.validationInterpreter.interpret = function(rules, methods) {
  @export
  */
 src.site.validation.validationInterpreter.interpretB = function(rules, methods) {
-    var methodsToCall = goog.array.map(rules, function(currentRule){
+    var methodsToCall = goog.array.map(rules, function(currentRule) {
         var propertyName = currentRule[0];
         var toCheck = goog.array.splice(currentRule, 1);
-        
-        var toCall = goog.array.map(toCheck, function(innerRule){
+
+        var toCall = goog.array.map(toCheck, function(innerRule) {
             var methodToUse = goog.array.find(methods, function(method) {
                 return method[0] === innerRule[0];
             })[1];
-             
+
             var arguments = goog.array.flatten([[propertyName], goog.array.slice(innerRule, 1)]);
-             
+
             return methodToUse(arguments);
         });
-        
+
         return toCall;
     });
-    
+
     return goog.array.flatten(methodsToCall);
 };
 
--- content/script/test/validation/when_interpreting_rules.js
+++ content/script/test/validation/when_interpreting_rules.js
@@ -8,117 +8,114 @@ goog.provide('src.test.validation.validationInterpreter.whenInterpretingRules');
 /**
  @export
  */
-src.test.validation.validationInterpreter.whenInterpretingRules.describe = function () {
-    //Using
-    var Current = src.site.validation.validationInterpreter;
+src.test.validation.validationInterpreter.whenInterpretingRules.describe = function() {
+  //Using
+  var Current = src.site.validation.validationInterpreter;
+  
+  //Fields
+  
+  var DefaultName_ = 'derp';
+  var DefaultUsername_ = 'herp';
+  var NameEmptyError_ = goog.string.getRandomString();
+  var UsernameEmptyError_ = goog.string.getRandomString();
+  var UsernameTooLongError_ = goog.string.getRandomString();
+  
+  var interpreter_;
+  var methodList_;
+  var rules_;
+  var toCheck_;
+  
+  //Test Hooks
+  beforeEach(function() {
     
-    //Fields
-    
-    var DefaultName_ = 'derp';
-    var DefaultUsername_ = 'herp';
-    var NameEmptyError_ = goog.string.getRandomString();
-    var UsernameEmptyError_ = goog.string.getRandomString();
-    var UsernameTooLongError_ = goog.string.getRandomString();
-    
-    var interpreter_;
-    var methodList_;
-    var rules_;
-    var toCheck_;
-    
-    //Test Hooks
-    beforeEach(function() {
+    var isNotEmpty = function(pair) {
+      return function(toCheck) {
+        var result = pair[1];
         
-        var isNotEmpty = function(pair){
-            return function(toCheck) {
-                var result = pair[1];
-                
-                if(!goog.string.isEmptySafe(toCheck[pair[0]])){
-                    result = '';
-                }
-                
-                return result;
-            };
-        };
+        if (!goog.string.isEmptySafe(toCheck[pair[0]])) {
+          result = '';
+        }
         
-        var isNotTooLong = function(settings){
-            return function(toCheck) {
-                var result = settings[2];
-                
-                if(toCheck[settings[0]].length < settings[1]) {
-                    result = '';
-                }
-                
-                return result;
-            };
-        };
-        
-        interpreter_ = {};
-        toCheck_ = {'Username': DefaultUsername_, 'Name': DefaultName_};
-        
-        methodList_ = [
-            ['is not empty', isNotEmpty],
-            ['is not too long', isNotTooLong]];
-        
-        
-        // rules_ = [
-        //     ['is not empty',
-        //      ['Username', UsernameEmptyError_],
-        //      ['Name', NameEmptyError_]],
-        //     ['is not too long',
-        //      ['Username', 6, UsernameTooLongError_]]];
-        
-        rules_ = [
-            ['Username',
-             ['is not empty', 'Username can not be empty'],
-             ['is not too long', 7, 'Username is too long.']],
-            ['Name',
-             ['is not empty', 'Name can not be empty.']]];
-        
-    });
-    
-    //Support Methods
-    var callTheMethod_ = function() {
-        var result = Current.interpretB(rules_, methodList_);
-        
-        return goog.array.map(result, function(currentItem) {
-            return currentItem(toCheck_);
-        });
+        return result;
+      };
     };
     
-    
-    //Test Methods
-    
-    it('should find an empty string', function() {
-        toCheck_ = {
-            'Name': '',
-            'Username': ''
-        };
+    var isNotTooLong = function(settings) {
+      return function(toCheck) {
+        var result = settings[2];
         
-        var result = callTheMethod_();
+        if (toCheck[settings[0]].length < settings[1]) {
+          result = '';
+        }
         
-        expect(goog.array.count(result, function(item) {
-            return item === UsernameEmptyError_ ||
-                item === NameEmptyError_;
-        })).toBe(2);
+        return result;
+      };
+    };
+    
+    interpreter_ = {};
+    toCheck_ = {'Username': DefaultUsername_, 'Name': DefaultName_};
+    
+    methodList_ = [
+      ['is not empty', isNotEmpty],
+      ['is not longer than', isNotTooLong]];
+    
+    rules_ = [
+      ['Username',
+       ['is not empty', UsernameEmptyError_],
+       ['is not longer than', 7, UsernameTooLongError_]],
+      ['Name',
+       ['is not empty', NameEmptyError_]]];
+    
+  });
+  
+  
+  //Support Methods
+  var callTheMethod_ = function() {
+    var result = Current.interpretB(rules_, methodList_);
+    
+    result = goog.array.map(result, function(currentItem) {
+      return currentItem(toCheck_);
     });
     
-    it('should find string that is too long', function() {
-        toCheck_ = {
-            'Name': 'hi',
-            'Username': '1234567'
-        };
-        
-        var result = callTheMethod_();
-        
-        expect(result.length === 1 &&
-               goog.array.count(result, function(item) {
-                   return item === UsernameTooLongError_;
-               })).toBe(1);
+    return goog.array.filter(result, function(currentItem){
+      return !goog.string.isEmptySafe(currentItem);
     });
+  };
+  
+  
+  //Test Methods
+  
+  it('should find an empty string', function() {
+    toCheck_ = {
+      'Name': '',
+      'Username': ''
+    };
     
+    var result = callTheMethod_();
+    
+    expect(goog.array.count(result, function(item) {
+      return item === UsernameEmptyError_ ||
+        item === NameEmptyError_;
+    })).toBe(2);
+  });
+  
+  it('should find string that is too long', function() {
+    toCheck_ = {
+      'Name': 'hi',
+      'Username': '1234567'
+    };
+    
+    var result = callTheMethod_();
+    
+    expect(result.length === 1 &&
+           goog.array.count(result, function(item) {
+             return item === UsernameTooLongError_;
+           })).toBe(1);
+  });
+
 };
 
 
 describe('When when, it', function() {
-    src.test.validation.validationInterpreter.whenInterpretingRules.describe();
+  src.test.validation.validationInterpreter.whenInterpretingRules.describe();
 });
