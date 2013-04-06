10348c8dc00686c7a23345aa23b9b8275502240f
sean <sean@byatool.com>
2013-04-06 08:26:59 -0700

    New version of the validation dsl is complete

--- content/script/live/validation/validationInterpreter.js
+++ content/script/live/validation/validationInterpreter.js
@@ -2,7 +2,6 @@ goog.require('goog.array');
 
 goog.provide('src.site.validation.validationInterpreter');
 
-
 /**
  @param {Array.<Array>} rules description.
  @param {Array.<function>} methods description.
@@ -10,48 +9,23 @@ goog.provide('src.site.validation.validationInterpreter');
  @export
  */
 src.site.validation.validationInterpreter.interpret = function(rules, methods) {
-    var methodsToCall = goog.array.map(rules, function(currentRule) {
-        var methodToUse = goog.array.find(methods, function(method) {
-            return method[0] === currentRule[0];
-        })[1];
+  var methodsToCall = goog.array.map(rules, function(currentRule) {
+    var propertyName = currentRule[0];
+    var toCheck = goog.array.splice(currentRule, 1);
 
-        var toCheck = goog.array.splice(currentRule, 1);
+    var toCall = goog.array.map(toCheck, function(innerRule) {
+      var methodToUse = goog.array.find(methods, function(method) {
+        return method[0] === innerRule[0];
+      })[1];
 
-        var toCall = goog.array.map(toCheck, function(innerRule) {
-            return methodToUse(innerRule);
-        });
+      var arguments = goog.array.flatten([[propertyName], goog.array.slice(innerRule, 1)]);
 
-        return toCall;
+      return methodToUse(arguments);
     });
 
-    return goog.array.flatten(methodsToCall);
-};
+    return toCall;
+  });
 
-
-/**
- @param {Array.<Array>} rules description.
- @param {Array.<function>} methods description.
- @return {fuction(Object) : Array.<string>} description.
- @export
- */
-src.site.validation.validationInterpreter.interpretB = function(rules, methods) {
-    var methodsToCall = goog.array.map(rules, function(currentRule) {
-        var propertyName = currentRule[0];
-        var toCheck = goog.array.splice(currentRule, 1);
-
-        var toCall = goog.array.map(toCheck, function(innerRule) {
-            var methodToUse = goog.array.find(methods, function(method) {
-                return method[0] === innerRule[0];
-            })[1];
-
-            var arguments = goog.array.flatten([[propertyName], goog.array.slice(innerRule, 1)]);
-
-            return methodToUse(arguments);
-        });
-
-        return toCall;
-    });
-
-    return goog.array.flatten(methodsToCall);
+  return goog.array.flatten(methodsToCall);
 };
 
