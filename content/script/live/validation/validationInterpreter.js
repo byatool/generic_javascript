0b67f25da1c7302d31bf560a7bac924859440e64
sean <sean@byatool.com>
2013-04-06 19:16:40 -0700

    Second round of test/refactor done on the validation dsl

--- content/script/live/validation/validationInterpreter.js
+++ content/script/live/validation/validationInterpreter.js
@@ -3,22 +3,50 @@ goog.require('goog.array');
 goog.provide('src.site.validation.validationInterpreter');
 
 /**
+ @param {Array} list This is the list to get the first member from.
+ @return {Object} The first member.
+ */
+src.site.validation.validationInterpreter.car = function(list) {
+  return list[0];
+};
+
+/**
+ @param {Array} list The list to get all but the first item from.
+ @return {Array} The created list.
+ */
+src.site.validation.validationInterpreter.cdr = function(list) {
+  return goog.array.slice(list, 1);
+};
+
+/**
  @param {Array.<Array>} rules description.
  @param {Array.<function>} methods description.
- @return {fuction(Object) : Array.<string>} description.
+ @param {?function(Array) : Object} car The method used to get the first of a list.
+ @param {?function(Array) : Array} cdr The method used to get all but the first item
+ in a list.
+ @param {?function(Array) : Array} flatten The method that will compress all method calls
+ into one method.
+ @return {Array.<function>} This is the list of validation methods that were
+ created.
  @export
  */
-src.site.validation.validationInterpreter.interpret = function(rules, methods) {
-  var methodsToCall = goog.array.map(rules, function(currentRule) {
-    var propertyName = currentRule[0];
-    var toCheck = goog.array.splice(currentRule, 1);
+src.site.validation.validationInterpreter.interpret = function(rules, methods, car, cdr, flatten) {
+  car = car ? car : src.site.validation.validationInterpreter.car;
+  cdr = cdr ? cdr : src.site.validation.validationInterpreter.cdr;
+  flatten = flatten ? flatten : goog.array.flatten;
+
+  //TODO create a testable method so that it can be injected... maybe
+  //  Too many untestable areas in this...
+  var methodGroups = goog.array.map(rules, function(currentRule) {
+    var propertyName = car(currentRule);
+    var toCheck = cdr(currentRule);
 
     var toCall = goog.array.map(toCheck, function(innerRule) {
       var methodToUse = goog.array.find(methods, function(method) {
-        return method[0] === innerRule[0];
+        return car(method) === car(innerRule);
       })[1];
 
-      var arguments = goog.array.flatten([[propertyName], goog.array.slice(innerRule, 1)]);
+      var arguments = goog.array.flatten([[propertyName], cdr(innerRule)]);
 
       return methodToUse(arguments);
     });
@@ -26,6 +54,6 @@ src.site.validation.validationInterpreter.interpret = function(rules, methods) {
     return toCall;
   });
 
-  return goog.array.flatten(methodsToCall);
+  return flatten(methodGroups);
 };
 
--- content/script/test/validation/when_interpreting_rules.js
+++ content/script/test/validation/when_interpreting_rules.js
@@ -11,107 +11,216 @@ goog.provide('src.test.validation.validationInterpreter.whenInterpretingRules');
 src.test.validation.validationInterpreter.whenInterpretingRules.describe = function() {
   //Using
   var Current = src.site.validation.validationInterpreter;
-  
+
   //Fields
-  
+
   var DefaultName_ = 'derp';
   var DefaultUsername_ = 'herp';
+
+  var IsNotEmpty_ = goog.string.getRandomString();
+  var IsNotLongerThan_ = goog.string.getRandomString();
+  var Name_ = goog.string.getRandomString();
   var NameEmptyError_ = goog.string.getRandomString();
+  var Username_ = goog.string.getRandomString();
   var UsernameEmptyError_ = goog.string.getRandomString();
   var UsernameTooLongError_ = goog.string.getRandomString();
-  
-  var interpreter_;
+
+  var car_;
+  var cdr_;
+  var flatten_;
+  var isNotEmpty_;
+  var isNotTooLong_;
   var methodList_;
   var rules_;
   var toCheck_;
-  
+
   //Test Hooks
   beforeEach(function() {
-    
-    var isNotEmpty = function(pair) {
+
+    isNotEmpty_ = function(pair) {
       return function(toCheck) {
         var result = pair[1];
-        
+
         if (!goog.string.isEmptySafe(toCheck[pair[0]])) {
           result = '';
         }
-        
+
         return result;
       };
     };
-    
-    var isNotTooLong = function(settings) {
+
+    isNotTooLong_ = function(settings) {
       return function(toCheck) {
         var result = settings[2];
-        
+
         if (toCheck[settings[0]].length < settings[1]) {
           result = '';
         }
-        
+
         return result;
       };
     };
-    
-    interpreter_ = {};
+
+    car_ = function(input) {
+      return src.site.validation.validationInterpreter.car(input);
+    };
+
+    cdr_ = function(input) {
+      return src.site.validation.validationInterpreter.cdr(input);
+    };
+
+    flatten_ = function() {
+      return [];
+    };
+
+
     toCheck_ = {'Username': DefaultUsername_, 'Name': DefaultName_};
-    
+
     methodList_ = [
-      ['is not empty', isNotEmpty],
-      ['is not longer than', isNotTooLong]];
-    
+      [IsNotEmpty_, isNotEmpty_],
+      [IsNotLongerThan_, isNotTooLong_]];
+
     rules_ = [
-      ['Username',
-       ['is not empty', UsernameEmptyError_],
-       ['is not longer than', 7, UsernameTooLongError_]],
-      ['Name',
-       ['is not empty', NameEmptyError_]]];
-    
+      [Username_,
+       [IsNotEmpty_, UsernameEmptyError_],
+       [IsNotLongerThan_, 7, UsernameTooLongError_]],
+      [Name_,
+       [IsNotEmpty_, NameEmptyError_]]];
+
   });
-  
-  
+
+
   //Support Methods
   var callTheMethod_ = function() {
-    var result = Current.interpretB(rules_, methodList_);
-    
+    var result = Current.interpret(rules_, methodList_, car_, cdr_, flatten_);
+
+    return result;
+
+  };
+
+
+  //Test Methods
+
+  it('should retrieve the property name for each rule.', function() {
+    var methodWasCalled = 0;
+
+    car_ = function(list) {
+      methodWasCalled += list === rules_[0] ||
+        list === rules_[1];
+      return src.site.validation.validationInterpreter.car(list);
+    };
+
+    callTheMethod_();
+
+    expect(methodWasCalled).toBe(2);
+  });
+
+
+  it('should retrieve the rest of the list.', function() {
+    var methodWasCalled = 0;
+
+    cdr_ = function(list) {
+      methodWasCalled += list === rules_[0] ||
+        list === rules_[1];
+      return src.site.validation.validationInterpreter.cdr(list);
+    };
+
+    callTheMethod_();
+
+    expect(methodWasCalled).toBe(2);
+  });
+
+
+  it('should find all the methods.', function() {
+    var methodWasCalled = false;
+    expect('this').toBe('tested when list methods are testable');
+  });
+
+
+  it('should use all the methods.', function() {
+    var methodWasCalled = 0;
+
+    isNotEmpty_ = function(otherArguments) {
+      methodWasCalled += goog.array.equals(otherArguments, [Username_, UsernameEmptyError_]) ||
+        goog.array.equals(otherArguments, [Name_, NameEmptyError_]);
+
+      return function() {};
+    };
+
+    isNotTooLong_ = function(otherArguments) {
+      methodWasCalled += goog.array.equals(otherArguments, [Username_, 7, UsernameTooLongError_]);
+      return function() {};
+    };
+
+    methodList_ = [
+      [IsNotEmpty_, isNotEmpty_],
+      [IsNotLongerThan_, isNotTooLong_]];
+
+    callTheMethod_();
+
+    expect(methodWasCalled).toBe(3);
+  });
+
+
+
+  it('should flatten the results.', function() {
+    var methodWasCalled = false;
+
+    flatten_ = function(finalList) {
+      methodWasCalled = true;
+      return [];
+    };
+
+    callTheMethod_();
+
+    expect(methodWasCalled).toBe(true);
+  });
+
+  it('should have the correct number of method calls returned.', function() {
+    flatten_ = goog.array.flatten;
+
+    var result = callTheMethod_();
+
+    expect(result.length).toBe(3);
+  });
+
+  //Temporary integration tests
+
+  it('should find an empty string', function() {
+    toCheck_ = {};
+    toCheck_[Name_] = '';
+    toCheck_[Username_] = '';
+
+    var result = Current.interpret(rules_, methodList_);
+
     result = goog.array.map(result, function(currentItem) {
       return currentItem(toCheck_);
     });
-    
-    return goog.array.filter(result, function(currentItem){
+
+    result = goog.array.filter(result, function(currentItem) {
       return !goog.string.isEmptySafe(currentItem);
     });
-  };
-  
-  
-  //Test Methods
-  
-  it('should find an empty string', function() {
-    toCheck_ = {
-      'Name': '',
-      'Username': ''
-    };
-    
-    var result = callTheMethod_();
-    
+
     expect(goog.array.count(result, function(item) {
       return item === UsernameEmptyError_ ||
         item === NameEmptyError_;
     })).toBe(2);
   });
-  
-  it('should find string that is too long', function() {
-    toCheck_ = {
-      'Name': 'hi',
-      'Username': '1234567'
-    };
-    
-    var result = callTheMethod_();
-    
-    expect(result.length === 1 &&
-           goog.array.count(result, function(item) {
-             return item === UsernameTooLongError_;
-           })).toBe(1);
-  });
+
+
+  // it('should find string that is too long', function() {
+  //   toCheck_ = {
+  //     'Name': 'hi',
+  //     'Username': '1234567'
+  //   };
+
+  //   var result = callTheMethod_();
+
+  //   expect(result.length === 1 &&
+  //          goog.array.count(result, function(item) {
+  //            return item === UsernameTooLongError_;
+  //          })).toBe(1);
+  // });
 
 };
 
