5582a4cf235430240f6bc53d860d949141f3b991
unknown <ssparling@INFOTK133.mctwf.org>
2013-04-16 13:20:56 -0400

    First half of redirect button list tested.

--- content/script/live/control/redirectList.js
+++ content/script/live/control/redirectList.js
@@ -1,6 +1,7 @@
 goog.require('goog.array');
+goog.require('goog.dom.forms');
 goog.provide('src.base.control.redirectList');
-
+goog.require('src.base.helper.domCreation');
 
 /**
  @const
@@ -63,23 +64,46 @@ src.base.control.redirectList.Goto = 'Goto';
  @param {Object} options The collection of options to build the customize the control.
  @param {?function(Object, Object) : Object} createADiv The method used to create a div element.
  @param {?function(Object, Object) : Object} createAButton The method used to create a button.
+ @param {?function(Object, string, string, function, function) : function} createTheClickEvent The method
+ compose the click handler for a created button.
+ @param {function(Object) : string} getValue The method used to get the value from the "sister" control.
+ @param {function(string)} redirect The method used to change the current url.
+ @param {?function(Object, Object)} setClick The method used to set the click event.
  @return {Object} The control.
  @export
  */
-src.base.control.redirectList.initialize = function(options, createADiv, createAButton) {
+src.base.control.redirectList.initialize = function(options, createADiv, createAButton, createTheClickEvent, getValue, redirect, setClick) {
     var Current = src.base.control.redirectList;
-    
-    //createADiv
+
+    createADiv = createADiv ? createADiv : src.base.helper.domCreation.createADiv;
+    createAButton = createAButton ? createAButton : src.base.helper.domCreation.button;
+
+    createTheClickEvent = createTheClickEvent ? createTheClickEvent : null;
+
+    getValue = getValue ? getValue : goog.dom.forms.getValue;
+    redirect = redirect ? redirect : window.location;
+    setClick = setClick ? setClick : src.base.helper.events.setClick;
+
+
     var container = createADiv({'id': options[Current.ContainerId], 'class': options[Current.ContainerClass]});
-    
-    goog.array.map(options[Current.ButtonList], function(currentItem){
+
+    var buttonList = goog.array.map(options[Current.ButtonList], function(currentItem) {
         var attributes = {};
-        
         attributes['id'] = currentItem[Current.ButtonId];
         attributes['value'] = currentItem[Current.ButtonText];
-        
-        return createAButton(attributes);
+
+        var createdButton = createAButton(attributes);
+
+        var clickEventHandler = createTheClickEvent(createdButton,
+                                                    currentItem[Current.For],
+                                                    currentItem[Current.Goto],
+                                                    getValue,
+                                                    redirect);
+
+        setClick(createdButton, clickEventHandler);
+
+        return createdButton;
     });
-    
+
     return container;
 };
--- content/script/test/control/redirectList/when_initializing_a_redirect_list.js
+++ content/script/test/control/redirectList/when_initializing_a_redirect_list.js
@@ -9,8 +9,8 @@ goog.provide('src.test.control.redirectList.whenInitializingARedirectList');
 src.test.control.redirectList.whenInitializingARedirectList.describe = function() {
     //Using
     var Current = src.base.control.redirectList;
-    
-    
+
+
     //Fields
     var ContainerClass_ = goog.string.getRandomString();
     var FirstButtonId_ = goog.string.getRandomString();
@@ -18,22 +18,28 @@ src.test.control.redirectList.whenInitializingARedirectList.describe = function(
     var FirstFor_ = goog.string.getRandomString();
     var FirstGoto_ = goog.string.getRandomString();
     var ParentContainerId_ = goog.string.getRandomString();
-    
+
     var SecondButtonId_ = goog.string.getRandomString();
     var SecondButtonText_ = goog.string.getRandomString();
     var SecondFor_ = goog.string.getRandomString();
     var SecondGoto_ = goog.string.getRandomString();
-    
+
     var buttonList_;
     var createAButton_;
     var createADiv_;
+    var createTheClickEvent_;
+    var firstButton_;
+    var getValue_;
     var options_;
     var parentContainer_;
-    
-    
+    var redirect_;
+    var secondButton_;
+    var setClickEvent_;
+
+
     //Test Hooks
     beforeEach(function() {
-        var createFakeButtonAttributes = function(id, text, controlId, url){
+        var createFakeButtonAttributes = function(id, text, controlId, url) {
             var button = {};
             button[Current.ButtonId] = id;
             button[Current.ButtonText] = text;
@@ -42,64 +48,128 @@ src.test.control.redirectList.whenInitializingARedirectList.describe = function(
 
             return button;
         };
-        
+
         parentContainer_ = {};
         options_ = {};
         options_[Current.ContainerClass] = ContainerClass_;
         options_[Current.ContainerId] = ParentContainerId_;
-        
+
         var firstButton = createFakeButtonAttributes(FirstButtonId_, FirstButtonText_, FirstFor_, FirstGoto_);
         var secondButton = createFakeButtonAttributes(SecondButtonId_, SecondButtonText_, SecondFor_, SecondGoto_);
         buttonList_ = [firstButton, secondButton];
         options_[Current.ButtonList] = buttonList_;
-        
-        createAButton_ = function() {};
+
+        var wasCalled = false;
+        firstButton_ = {};
+        secondButton_ = {};
+        createAButton_ = function() {
+            var result = wasCalled ? secondButton_ : firstButton_;
+            wasCalled = true;
+
+            return result;
+        };
+
         createADiv_ = function() { return parentContainer_; };
+        createTheClickEvent_ = function() {};
+        getValue_ = function() { };
+        redirect_ = function() {};
+        setClickEvent_ = function() {};
     });
-    
-    
+
+
     //Support Methods
     var callTheMethod_ = function() {
-        return Current.initialize(options_, createADiv_, createAButton_);
+        return Current.initialize(options_, createADiv_, createAButton_, createTheClickEvent_, getValue_, redirect_, setClickEvent_);
     };
-    
-    
+
+
     //Test Methods
-    
+
     it('should create a parent container.', function() {
         var methodWasCalled = false;
-        
+
         createADiv_ = function(attributes) {
             methodWasCalled = attributes['id'] === options_[Current.ContainerId] &&
                 attributes['class'] === options_[Current.ContainerClass];
-            
+
             return parentContainer_;
         };
         callTheMethod_();
-        
+
         expect(methodWasCalled).toBe(true);
     });
-    
-    
+
+
     it('should return the parent container.', function() {
         expect(callTheMethod_()).toBe(parentContainer_);
     });
-    
-    
+
+
     it('should create the needed buttons.', function() {
         var methodWasCalled = 0;
-        
-        createAButton_ = function(attributes){
+
+        createAButton_ = function(attributes) {
             methodWasCalled += (attributes['id'] === FirstButtonId_ &&
                                 attributes['value'] === FirstButtonText_) ||
                 (attributes['id'] === SecondButtonId_ &&
                  attributes['value'] === SecondButtonText_);
         };
-        
+
         callTheMethod_();
-        
+
         expect(methodWasCalled).toBe(2);
     });
+
+
+    it('should create the click event for all buttons.', function() {
+        var methodWasCalled = 0;
+
+        createTheClickEvent_ = function(button, elementId, url, getValue, redirect) {
+
+            methodWasCalled += (button === firstButton_ &&
+                                elementId === FirstFor_ &&
+                                url === FirstGoto_ &&
+                                getValue === getValue_ &&
+                                redirect === redirect_) ||
+                (button === secondButton_ &&
+                 elementId === SecondFor_ &&
+                 url === SecondGoto_ &&
+                 getValue === getValue_ &&
+                 redirect === redirect_);
+        };
+
+        callTheMethod_();
+
+        expect(methodWasCalled).toBe(2);
+    });
+
+
+    it('should set the click event on the button.', function() {
+        var methodWasCalled = 0;
+        var wasCalled = false;
+        var firstEvent = {};
+        var secondEvent = {};
+
+        createTheClickEvent_ = function() {
+            var result = wasCalled ? secondEvent : firstEvent;
+            wasCalled = true;
+
+            return result;
+        };
+
+        setClickEvent_ = function(button, method) {
+            methodWasCalled += (button === firstButton_ &&
+                               method === firstEvent) ||
+                (button === secondButton_ &&
+                 method === secondEvent);
+
+        };
+
+        callTheMethod_();
+
+        expect(methodWasCalled).toBe(2);
+    });
+
 };
 
 
