afced066c20868bbc36a7c460481f990da3505d1
unknown <ssparling@INFOTK226.mctwf.org>
2013-05-13 13:38:47 -0400

    Initialize simpleInformationDisplay is for really done this time.

--- content/script/live/control/simpleInformationDisplay.js
+++ content/script/live/control/simpleInformationDisplay.js
@@ -1,11 +1,11 @@
 /*
- 
+
  options['ContainerClass']
  options['RowClass']
  options['LabelClass']
  options['ValueClass']
  options['Information',  {'name': 'FirstName', 'label': 'First Name'}]
- 
+
  - <div class="containerClass">
  -  <div class="rowClass">
  -    <div class="labelClass">
@@ -20,6 +20,8 @@
 goog.require('goog.array');
 goog.require('goog.dom');
 goog.provide('src.base.control.simpleInformationDisplay');
+goog.require('src.base.helper.domHelper');
+
 
 /* Fields */
 
@@ -45,36 +47,67 @@ src.base.control.simpleInformationDisplay.ContainerId = 'ContainerId';
 src.base.control.simpleInformationDisplay.LayoutItems = 'LayoutItems';
 
 
+
+/**
+ @param {Object} container  The parent container that houses the rows to fill.
+ @param {function(Object, Array.<Object>} fillTheRows The function used to fill
+ a container.
+ @return {function(Object)} The function used to update a select from a result.
+ @protected
+ */
+src.base.control.simpleInformationDisplay.createRowsHandler = function(container, fillTheRows) {
+  return function(result) {
+    fillTheRows(container, result);
+  };
+};
+
+/**
+ @param {Object} result The result with the retrieved information.
+ @protected
+ */
+src.base.control.simpleInformationDisplay.fillTheRows = function(result) {
+
+};
+
+
 /* Exports */
 
 /**
+ @param {string} url The url to retrieve the needed info.
+ @param {Array.<string>} parameters The request string parameters.
  @param {Object} options The is the set of options for building.
  @param {?function(Object, Object) : Object} createADiv The method used to create a div element.
  @param {?function(Object) : Object} createLayoutItem description.
  @param {?function(Object, Object)} appendChild The method used to append a child to a parent element.
+ @param {?function(Object, function) : function(Object}) createTheCallBack a.
+ @param {?function(string, Object, function)} submitData a.
+ @param {?function} fillTheRows a.
  @return {Object} The created display.
  @export
  */
-src.base.control.simpleInformationDisplay.initialize = function(options, createADiv, createLayoutItem, appendChild){
+src.base.control.simpleInformationDisplay.initialize = function(url, parameters, options, createADiv, createLayoutItem, appendChild, createTheCallBack, submitData, fillTheRows) {
   var Current = src.base.control.simpleInformationDisplay;
-  
+
+  appendChild = appendChild ? appendChild : goog.dom.appendChild;
   createADiv = createADiv ? createADiv : src.base.helper.domCreation.div;
   createLayoutItem = createLayoutItem ? createLayoutItem : null;
-  appendChild = appendChild ? appendChild : goog.dom.appendChild;
+  createTheCallBack = createTheCallBack ? createTheCallBack : src.base.control.simpleInformationDisplay.createRowsHandler;
+  fillTheRows = fillTheRows ? fillTheRows : src.base.control.simpleInformationDisplay.fillTheRows;
+  submitData = submitData ? submitData : src.base.helper.domHelper.submitToUrl;
 
 
   var parentContainer = createADiv({'id': options[Current.ContainerId], 'class': options[Current.ContainerClass]});
-  
-  var rows = goog.array.map(options[Current.LayoutItems], function(currentItem){
+
+  var rows = goog.array.map(options[Current.LayoutItems], function(currentItem) {
     return createLayoutItem(currentItem);
   });
-  
+
   goog.array.forEach(rows, function(item) {
     appendChild(parentContainer, item);
   });
-  
-  //Set off the async call
-  //return the control
+
+  submitData(url, parameters, createTheCallBack(parentContainer, fillTheRows));
+
   return parentContainer;
 };
 
--- content/script/test/control/simpleInformationDisplay/when_initializing_a_simple_information_display.js
+++ content/script/test/control/simpleInformationDisplay/when_initializing_a_simple_information_display.js
@@ -6,105 +6,153 @@ goog.provide('src.test.control.simpleInformationDisplay.whenInitializingASimpleI
 /**
  @export
  */
-src.test.control.simpleInformationDisplay.whenInitializingASimpleInformationDisplay.describe = function () {
+src.test.control.simpleInformationDisplay.whenInitializingASimpleInformationDisplay.describe = function() {
   //Using
+
   var Current = src.base.control.simpleInformationDisplay;
-  
-  
+
+
   //Fields
+
   var ParentContainerClass_ = goog.string.getRandomString();
   var ParentContainerId_ = goog.string.getRandomString();
+  var Url_ = goog.string.getRandomString();
 
   var appendChild_;
   var containerDiv_;
   var createADiv_;
   var createLayoutItem_;
+  var createTheCallBack_;
+  var fillTheRows_;
   var layoutItems_;
   var options_;
+  var parameters_;
   var parentContainer_;
-  
-  
+  var submitData_;
+
+
   //Test Hooks
   beforeEach(function() {
-    parentContainer_ = {};
     options_ = {};
+    parameters_ = {};
+    parentContainer_ = {};
+
     options_[Current.ContainerId] = ParentContainerId_;
     options_[Current.ContainerClass] = ParentContainerClass_;
-    
-    layoutItems_ = [{'userName':'Username:'}, {'password':'Password'}];
+
+    layoutItems_ = [{'userName': 'Username:'}, {'password': 'Password'}];
     options_[Current.LayoutItems] = layoutItems_;
-    
+
     containerDiv_ = {};
-    
+
+    appendChild_ = function() {};
     createADiv_ = function() { return containerDiv_; };
     createLayoutItem_ = function() { return {}; };
-    appendChild_ = function() {};
+    createTheCallBack_ = function() {};
+    fillTheRows_ = function() {};
+    submitData_ = function() {};
   });
-  
-  
+
+
   //Support Methods
   var callTheMethod_ = function() {
-    return Current.initialize(options_, createADiv_, createLayoutItem_, appendChild_);
+    return Current.initialize(Url_, parameters_, options_, createADiv_, createLayoutItem_, appendChild_,
+                              createTheCallBack_, submitData_, fillTheRows_);
   };
-  
-  
+
+
   //Test Methods
-  
+
   it('should create a parent container.', function() {
     var methodWasCalled = false;
-    
+
     createADiv_ = function(attributes) {
       methodWasCalled = attributes['id'] === options_[Current.ContainerId] &&
         attributes['class'] === options_[Current.ContainerClass];
       return parentContainer_;
     };
     callTheMethod_();
-    
+
     expect(methodWasCalled).toBe(true);
   });
-  
-  
+
+
   it('should create a layout row for each item.', function() {
     var methodWasCalled = 0;
-    
+
     createLayoutItem_ = function(item) {
       methodWasCalled += item === layoutItems_[0] ||
         item === layoutItems_[1];
     };
-    
+
     callTheMethod_();
-    
+
     expect(methodWasCalled).toBe(2);
   });
-  
-  
+
+
   it('should append each created row.', function() {
     var callCount = 0;
     var firstItem = {};
     var methodWasCalled = 0;
     var secondItem = {};
-    
+
     createLayoutItem_ = function() {
       callCount += 1;
       return callCount < 2 ? firstItem : secondItem;
     };
-    
+
     appendChild_ = function(parentContainer, item) {
       methodWasCalled += parentContainer === containerDiv_ &&
         (item === firstItem || item === secondItem);
     };
-    
+
     callTheMethod_();
-    
+
     expect(methodWasCalled).toBe(2);
   });
 
-  
+
+  //
+
+  it('should create the result handling method.', function() {
+    var methodWasCalled = false;
+
+    createTheCallBack_ = function(container, methodToUse) {
+      methodWasCalled = container === containerDiv_ &&
+        methodToUse === fillTheRows_;
+    };
+    // su
+
+    callTheMethod_();
+
+    expect(methodWasCalled).toBe(true);
+  });
+
+
+  it('should call to retrieve the information, and update the container.', function() {
+    var methodWasCalled = false;
+    var finalMethod = {};
+
+    createTheCallBack_ = function() {
+      return finalMethod;
+    };
+
+    submitData_ = function(url, parameters, fillTheList) {
+        methodWasCalled = url === Url_ &&
+          parameters === parameters_ &&
+        fillTheList === finalMethod;
+    };
+
+    callTheMethod_();
+
+    expect(methodWasCalled).toBe(true);
+  });
+
   it('should return the container.', function() {
-    
     expect(callTheMethod_()).toBe(containerDiv_);
   });
-  
+
 };
 
 
